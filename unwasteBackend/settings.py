e42dfdf2d0cc677eba53ce798851c43f6990c889
jacksonmgrant <jacksonmgrant@gmail.com>
2023-11-06 13:57:38 -0600

    Make migration, allow all hosts

--- unwasteBackend/settings.py
+++ unwasteBackend/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-gte!5%&-!ifbtl^f+0kop=%frpvsjix-2uhb07-sjq!5@n50=u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-developer_ip = '172.17.53.88'
+developer_ip = '192.168.1.230'
 
-ALLOWED_HOSTS = [developer_ip, 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
