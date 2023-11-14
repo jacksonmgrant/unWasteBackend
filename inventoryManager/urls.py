8eb0c4bd0a5840b01af8c6c207f8f96c51aaecd3
abrown104 <anna-r-brown@uiowa.edu>
2023-11-14 12:32:47 -0600

    Anna's myenv to run server

--- inventoryManager/urls.py
+++ inventoryManager/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('api/questions/', views.QuestionList.as_view(), name='question-list'),
     path('api/questions/<int:pk>/', views.QuestionList.as_view(), name='question-detail'),
 ]
+
+#http://192.168.4.24:8080/inventoryManager/api/questions/
